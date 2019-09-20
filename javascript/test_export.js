35ee665ba8fa812fe0b28e4a77c904e7175269b6
combofish <18233092052@139.com>
2019-09-20 10:24:43 +0800

    test nodejs exports

---
+++ javascript/test_export.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+// coding:utf-8
+// Filename: test_export.js
+'use strict'
+
+exports.cry = function(){
+    console.log('cry')
+}
+
+exports.smeil = function(){
+    console.log('smeil')
+}
+
+exports.turn = () => {
+    console.log('smeil and cry!')
+}
---
+++ javascript/test_export_2.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+// coding:utf-8
+// Filename: test_export_2.js
+'use strict'
+
+
+var use_test = require('./test_export.js')
+
+use_test.cry()
+use_test.smeil()
+use_test.turn()
