9d6b67d9fa14f35ba5e3d5b9527e25d5fc051a4b
combofish <18233092052@139.com>
2020-06-06 23:12:50 +0800

    add dht11.c and datatype.h in MCU_C51

---
+++ MCU_CODE/MCU_C51/datatype.h
@@ -0,0 +1,20 @@
+#ifndef		__DATATYPE_H
+#define		__DATATYPE_H
+
+/*********************************************************/
+
+typedef bit BOOL;	//位变量
+typedef unsigned char 	INT8U;//无符号8位整型变量
+typedef signed char		INT8S;//有符号8位整型变量
+typedef unsigned int 	INT16U;//无符号16位整型变量
+typedef signed int		INT16S;//有符号16位整型变量
+typedef unsigned long	INT32U;//无符号32位整型变量
+typedef signed long		INT32S;//有符号32位整型变量
+typedef float			FP32;  //单精度浮点数（32位长度）
+typedef double			FP64;  //双精度浮点数（64位长度）	
+
+typedef unsigned char 	uchar; //无符号8位整型变量
+typedef unsigned int	uint;  //无符号16位整型变量
+/*********************************************************/
+
+#endif
